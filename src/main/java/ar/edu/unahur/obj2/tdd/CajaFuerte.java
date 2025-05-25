c9f1b69567f5e7a513794358b7fbea6c10427bd8
Matu <matiasdavidt@gmail.com>
2025-05-25 20:53:44 -0300

    cambio final

--- src/main/java/ar/edu/unahur/obj2/tdd/CajaFuerte.java
+++ src/main/java/ar/edu/unahur/obj2/tdd/CajaFuerte.java
@@ -1,5 +1,31 @@
 package ar.edu.unahur.obj2.tdd;
 
 public class CajaFuerte {
-
+	
+	private Integer clave = 1234;
+	
+	private Boolean estado;
+	
+	public CajaFuerte(){
+		this.estado = Boolean.TRUE;
+	}
+	
+	public void setClave(Integer nuevaClave) {
+		this.clave = nuevaClave;
+	}
+	
+	public Boolean estado() {
+		return estado;
+	}
+	
+	public void abrir(Integer claveCaja) {
+		if (this.clave.equals(claveCaja)){
+				this.estado = Boolean.TRUE;
+		}
+	}
+	
+	public void cerrar(Integer claveApertura) {
+		this.estado = Boolean.FALSE;
+		this.setClave(claveApertura);
+	}
 }
--- src/test/java/ar/edu/unahur/obj2/tdd/CajaFuerteTest.java
+++ src/test/java/ar/edu/unahur/obj2/tdd/CajaFuerteTest.java
@@ -1,10 +1,38 @@
 package ar.edu.unahur.obj2.tdd;
 
+import org.junit.jupiter.api.Test;
+
+import ar.edu.unahur.obj2.tdd.*;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
 
 public class CajaFuerteTest {
 
+	@Test
+	void abrirCajaFuerte() {
+		
+		var caja = new CajaFuerte();
+		
+		caja.cerrar(1234);
+		caja.abrir(1234);
+		
+		assertTrue(caja.estado());
+	}
+	
+	@Test
+	void cerrarCajaFuerteEstablecerNuevaClaveIntentarAbrirConClaveViejayLuegoNueva() {
+		
+		var caja = new CajaFuerte();
+		
+		caja.cerrar(6789);
+		
+		caja.abrir(1234);	
+		assertEquals(Boolean.FALSE, caja.estado());
+		
+		caja.abrir(6789);
+		assertEquals(Boolean.TRUE, caja.estado());
+	}
 }
